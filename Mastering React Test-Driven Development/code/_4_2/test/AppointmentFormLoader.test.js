d93cd040e3d470c6a3f890111608bc48dcde14ff
HyeongJinK <wkzkfmxk23@gmail.com>
2020-10-21 18:33:49 +0900

    4_2 study ing...

---
+++ Mastering React Test-Driven Development/code/_4_2/test/AppointmentFormLoader.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'whatwg-fetch';
+import { createContainer } from './domManipulators';
+import { fetchResponseOk } from './spyHelpers';
+import {
+  AppointmentFormLoader
+} from '../src/AppointmentFormLoader';
+
+describe('AppointmentFormLoader', () => {
+  let render, container;
+
+  const today = new Date();
+  const availableTimeSlots = [
+    { startsAt: today.setHours(9, 0, 0, 0) }
+  ];
+
+  beforeEach(() => {
+    ({ render, container } = createContainer());
+    jest
+      .spyOn(window, 'fetch')
+      .mockReturnValue(fetchResponseOk(availableTimeSlots));
+  });
+
+  afterEach(() => {
+    window.fetch.mockRestore();
+  });
+});
