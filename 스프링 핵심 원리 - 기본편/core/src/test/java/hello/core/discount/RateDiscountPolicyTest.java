6466496a447d18d0872007bb0555aad3e37d3f9b
napasun <wkzkfmxk23@gmail.com>
2020-10-27 02:10:18 +0900

    잘못 작성된 테스트 수정

--- 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -21,7 +21,7 @@ class RateDiscountPolicyTest {
     @Test
     @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
     void vip_x() {
-        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
         int discount = discountPolicy.discount(member, 10000);
         Assertions.assertThat(discount).isEqualTo(0);
     }
